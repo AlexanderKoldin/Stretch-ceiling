62eaef9a5e3a8564b2cfcadbecf0a08d8e939cb7
Alexander Koldin <koldinweb@gmail.com]>
2024-01-19 12:06:25 +0300

    Сделал свайпер

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log('Hello world');
+const swiper = new Swiper('.swiper', {
+	direction: 'horizontal',
+	slidesPerView: 6,
+	spaceBetween: 0,
+	speed: 10000,
+	effect: 'slide',
+	loop: true,
+	autoplay: {
+		delay: 0, // Задержка в миллисекундах между переходами
+		disableOnInteraction: false, // Не останавливать автовоспроизведение при взаимодействии пользователя
+	},
+});
